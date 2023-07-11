09d5884fa7741ef6935f422a0aab3461e08e640a
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-07-11 09:30:42 +0000

    Resolve "Create factory and seeder for the eloquent models"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
+use App\Models\SubTask;
+use App\Models\Task;
+use App\Models\User;
+use App\Models\UserDetail;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(10)->create();
+        SubTask::truncate();
+        Task::truncate();
+        Project::truncate();
+        UserDetail::truncate();
+        User::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(20)->create()->each(function ($user) {
+            UserDetail::factory(1)->create([
+                'username' => $user->username,
+            ]);
+
+            Project::factory(2)->create([
+                'owner_id' => $user->id,
+            ])->each(function ($project) {
+                Task::factory(4)->create([
+                    'project_id' => $project->id,
+                    'creator_id' => $project->owner_id,
+                    'assigned_to_id' => $project->owner_id,
+                ])->each(function ($task) {
+                    SubTask::factory(2)->create([
+                        'task_id' => $task->id,
+                    ]);
+                });
+
+            });
+        });
     }
 }
