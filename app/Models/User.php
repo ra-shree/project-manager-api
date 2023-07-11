4e0c1a16ab2bfce574eaeb841c64ceed27553289
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-07-11 10:41:37 +0545

    refactor:offloaded additional user information to user_details table

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -21,10 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'phone_number',
         'username',
-        'agreement',
-        'gender'
     ];
 
     /**
@@ -46,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function userDetails(): hasOne
+    {
+        return $this->hasOne(UserDetail::class);
+    }
 }
