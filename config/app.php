8cbe5b70b40e7139cc174ea964fff7251c6ea7c6
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-07-24 16:55:24 +0545

    feat:removed Spatie and added Token auth

--- config/app.php
+++ config/app.php
@@ -59,6 +59,8 @@ return [
 
     'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
     'asset_url' => env('ASSET_URL'),
 
     /*
@@ -170,7 +172,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
