9335feb822a498755417b990fcfb5e4113e12c9c
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-07-28 13:46:08 +0545

    fix:wrong import in AuthenticatedSessionController

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use bootstrap\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
